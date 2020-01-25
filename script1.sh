e6d3012d14f4ae31f5dc431efa21d6729134fecb
Saiteju1997 <51901826+Saiteju1997@users.noreply.github.com>
2020-01-25 16:43:55 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /inet
-docker build -t $JOB_NAME.V1.$BUILD_ID .
-docker tag $JOB_NAME.V1.$BUILD_ID steju480/$JOB_NAME.V1.$BUILD_ID
-docker tag $JOB_NAME.V1.$BUILD_ID steju480/$JOB_NAME.V1:latest
-docker push steju480/Docker_trail.V1.$BUILD_ID
-docker push steju480/Docker_trail.V1:latest
-docker rmi steju480/$JOB_NAME.V1.$BUILD_ID
-docker rmi steju480/$JOB_NAME.V1:latest
+docker build -t $JOB_NAME.v1.$BUILD_ID .
+docker tag $JOB_NAME.v1.$BUILD_ID steju480/$JOB_NAME.v1.$BUILD_ID
+docker tag $JOB_NAME.v1.$BUILD_ID steju480/$JOB_NAME.v1:latest
+docker push steju480/Docker_trail.v1.$BUILD_ID
+docker push steju480/Docker_trail.v1:latest
+docker rmi steju480/$JOB_NAME.v1.$BUILD_ID
+docker rmi steju480/$JOB_NAME.v1:latest
 
