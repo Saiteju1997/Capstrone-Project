83f06ae954866f32116f6a2d2a1760bfb6fc4813
root <root@docker-master.us-central1-a.c.rosy-chess-243411.internal>
2020-01-25 09:58:30 +0000

    adding script file

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /root
+docker build -t $JOB_NAME.V1.$BUILD_ID . .
+docker tag $JOB_NAME.V1.$BUILD_ID steju480/$JOB_NAME.V1.$BUILD_ID
+docker tag $JOB_NAME.V1.$BUILD_ID steju480/$JOB_NAME.V1:latest
+docker push steju480/Docker_trail.V1.$BUILD_ID
+docker push steju480/Docker_trail.V1:latest
+docker rmi steju480/$JOB_NAME.V1.$BUILD_ID
+docker rmi steju480/$JOB_NAME.V1:latest
+
